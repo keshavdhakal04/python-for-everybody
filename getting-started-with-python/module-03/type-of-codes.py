e9a38ac4d1d30e7752e54098f4ac03691144ecbe
Keshav Dhakal <keshavdhakal04@gmail.com>
2026-01-22 18:48:49 -0500

    completed module 03

---
+++ getting-started-with-python/module-03/type-of-codes.py
@@ -0,0 +1,21 @@
+#sequential code
+x = 1
+print(x)
+x = x + 1
+print(x)
+print('finish')
+
+#conditional code
+x = 5
+if x < 10:
+    print('smaller')
+if x > 20:
+    print('bigger')
+print('finish')
+
+#repeated code (loop)
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print('finish')
